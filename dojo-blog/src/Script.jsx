c5989b93e4e8536f8148e5bde7913c88015a3dd6
Brian Simiyu <briansimiyuj@gmail.com>
2023-06-28 10:42:52 +0300

    Multiple Components

--- dojo-blog/src/Script.jsx
+++ dojo-blog/src/Script.jsx
@@ -1,48 +1,18 @@
-import './App.css';
+import './App.css'
+
+import NavBar from './NavBar'
 
 function App() {
-
-  const title = 'Welcome to the new blog!',
-
-  likes = 50,
-
-  person = {
-
-    name  : 'John',
-
-    age: 30
-
-  },
-
-  isOpen = false,
-
-  link = 'http://techfybiz.co.ke'
 
 
   return (
     <div className="App">
+
+      <NavBar/>
       
       <div className="content">
 
-        <h1>{title}</h1>
-
-        <p>Liked {likes} times</p>
-
-        {/* <p>{person}</p> */}
-
-        {/* <p>{isOpen}</p> */}
-
-        <p>My name is {person.age} and I am {person.age} old</p>
-
-        <p>{10}</p>
-
-        <p>{'Hello Ninjas'}</p>
-
-        <p>{[1, 2, 3, 4, 5]}</p>
-
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>TechFyBiz</a>
+        <h1>App Component</h1>
 
       </div>
 
