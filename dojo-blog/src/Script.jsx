df4e043a25911de4f7133835243cba6a6baecba7
Brian Simiyu <briansimiyuj@gmail.com>
2023-06-28 10:04:47 +0300

    Dynamic values

--- dojo-blog/src/Script.jsx
+++ dojo-blog/src/Script.jsx
@@ -1,12 +1,44 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new blog!',
+
+  likes = 50,
+
+  person = {
+
+    name  : 'John',
+
+    age: 30
+
+  },
+
+  isOpen = false
+
+
   return (
     <div className="App">
       
       <div className="content">
 
-        <h1>Hello World</h1>
+        <h1>{title}</h1>
+
+        <p>Liked {likes} times</p>
+
+        {/* <p>{person}</p> */}
+
+        {/* <p>{isOpen}</p> */}
+
+        <p>My name is {person.age} and I am {person.age} old</p>
+
+        <p>{10}</p>
+
+        <p>{'Hello Ninjas'}</p>
+
+        <p>{[1, 2, 3, 4, 5]}</p>
+
+        <p>{Math.random() * 10}</p>
 
       </div>
 
