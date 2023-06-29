62169c677c455edaed3265a5ad282f7ae22a2c1a
Brian Simiyu <briansimiyuj@gmail.com>
2023-06-29 14:17:22 +0300

    Click Events

--- dojo-blog/src/Home.jsx
+++ dojo-blog/src/Home.jsx
@@ -1,10 +1,18 @@
 const Home = () =>{
 
+    const handleClick = () =>{
+    
+       console.log('working')
+    
+    }
+
     return(
 
         <div className="home">
 
             <h2>Homepage</h2>
+
+            <button onClick={handleClick}>Click me</button>
 
         </div>
 
--- dojo-blog/src/NavBar.jsx
+++ dojo-blog/src/NavBar.jsx
@@ -11,11 +11,7 @@ const NavBar = () =>{
 
                 <a href="/">Home</a>
 
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px"
-                }}>New Blog</a>
+                <a href="/create">New Blog</a>
                 
             </div>
             
