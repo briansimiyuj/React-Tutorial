8f81aed59154e5a68cccdb581af0b6a2d508ed85
Brian Simiyu <briansimiyuj@gmail.com>
2023-07-10 10:26:53 +0300

    Props

--- dojo-blog/src/Home.jsx
+++ dojo-blog/src/Home.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import BlogList from './BlogList'
+
 const Home = () =>{ 
 
     const [blogs, setBlogs] = useState([
@@ -17,21 +19,7 @@ const Home = () =>{
 
         <div className="home">
 
-           {
-
-                blogs.map(blog =>(
-
-                    <div className="blog-preview" key={blog.id}>
-
-                        <h2>{blog.title}</h2>
-
-                        <p>Written by {blog.author}</p>
-
-                    </div>
-
-                ))
-
-           } 
+          <BlogList blogs={blogs} title='All blogs!'/>
 
         </div>
 
