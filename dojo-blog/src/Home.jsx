e5309a1233cae6593990b213ce5e48d1f547827e
Brian Simiyu <briansimiyuj@gmail.com>
2023-07-10 09:32:20 +0300

    Outputting lists

--- dojo-blog/src/Home.jsx
+++ dojo-blog/src/Home.jsx
@@ -2,29 +2,36 @@ import { useState } from 'react'
 
 const Home = () =>{ 
 
-    const [name, setName] = useState('Brian')
+    const [blogs, setBlogs] = useState([
 
-    const [age, setAge] = useState(27)
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
 
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
 
-    const handleClick = () =>{
-    
-       setName('Simiyu')
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
 
-       setAge(30)
-        
-    }
+    ])
 
 
     return(
 
         <div className="home">
 
-            <h2>Homepage</h2>
+           {
 
-            <p>{name} is {age} years old</p>
+                blogs.map(blog =>(
 
-            <button onClick={handleClick}>Click me</button>
+                    <div className="blog-preview" key={blog.id}>
+
+                        <h2>{blog.title}</h2>
+
+                        <p>Written by {blog.author}</p>
+
+                    </div>
+
+                ))
+
+           } 
 
         </div>
 
