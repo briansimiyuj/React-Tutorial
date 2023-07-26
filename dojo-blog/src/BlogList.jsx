510bc8d3644cfeb94faa4804dee69fb312ccc620
Brian Simiyu <briansimiyuj@gmail.com>
2023-07-26 12:31:14 +0300

    Add dynamic blog link

--- dojo-blog/src/BlogList.jsx
+++ dojo-blog/src/BlogList.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 const BlogList = ( {blogs, title} ) =>{
 
     return(
@@ -14,9 +16,13 @@ const BlogList = ( {blogs, title} ) =>{
 
                     <div className="blog-preview" key={blog.id}>
 
-                        <h2>{blog.title}</h2>
+                        <Link to={`/blogs/${blog.id}`}>
+                        
+                            <h2>{blog.title}</h2>
 
-                        <p>Written by {blog.author}</p>
+                            <p>Written by {blog.author}</p>
+                        
+                        </Link>
 
                     </div>
 
