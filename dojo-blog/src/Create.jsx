dc16728fad3a3b39903c68fe66abc2b33e90328c
Brian Simiyu <briansimiyuj@gmail.com>
2023-07-27 11:28:57 +0300

    Added a form

--- dojo-blog/src/Create.jsx
+++ dojo-blog/src/Create.jsx
@@ -6,6 +6,35 @@ const Create = () =>{
 
             <h2>Add a New Blog</h2>
 
+
+            <form>
+
+                <label>Blog title:</label>
+
+                <input type="text" required />
+
+
+                <label>Blog body:</label>
+
+                <textarea required></textarea>
+
+
+                <label>Blog author:</label>
+
+                
+                <select>
+
+                    <option value="mario">Mario</option>
+
+                    <option value="yoshi">Yoshi</option>
+
+                </select>
+
+
+                <button>Add Blog</button>
+
+            </form>
+
         </div>
 
     )
