910f812f38bb34ed78205b1a0bc764eb51ed433a
Brian Simiyu <briansimiyuj@gmail.com>
2023-07-31 12:16:37 +0300

    Not found page

--- dojo-blog/src/Script.jsx
+++ dojo-blog/src/Script.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Create from './Create'
 
 import BlogDetails from './BlogDetails'
+import NotFound from './NotFound'
 
 function App() {
 
@@ -40,6 +41,13 @@ function App() {
             <Route path="/blogs/:id">
 
               <BlogDetails/>
+
+            </Route>
+
+
+            <Route path="*">
+
+              <NotFound/>
 
             </Route>
 
